a286df3aed35a0e40b2217c77b1c7d609e42c634
LD <LDinho@users.noreply.github.com>
2019-06-29 10:32:15 -0400

    add tests for get users route

--- api/server.test.js
+++ api/server.test.js
@@ -23,15 +23,42 @@ describe('server', () =>{
     it('should return status 200', async () => {
       const res = await request(server).get('/');
       expect(res.status).toBe(200);
-    })
+    });
+
     it('should return json', async () => {
       const res = await request(server).get('/');
       expect(res.type).toBe('application/json');
-    })
+    });
+
     it('should return { api: "up"} for the root page', async () => {
       const res = await request(server).get('/');
       expect(res.body).toEqual({ api: "up"});
-    })
-  })
+    });
 
-})
+  });
+
+  describe('GET /users', () => {
+    it('should return json', async () => {
+      const res = await request(server).get('/users');
+      expect(res.status).toBe(200);
+      expect(res.type).toBe('application/json');
+      expect(res.body).toEqual([]);
+    });
+
+    it('should get a res with all users from db', async () => {
+      await db('users').insert([
+        {name: 'joe'},
+        {name: 'snow'},
+      ])
+
+      const res = await request(server).get('/users');
+      expect(res.body.length).toBe(2)
+      expect(res.status).toBe(200);
+      expect(res.body[0].name).toBe('joe')
+      expect(res.body[0].id).toBe(1)
+      expect(res.type).toBe('application/json');
+
+    });
+  });
+
+});
