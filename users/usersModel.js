9447a73e89dac202a2eac37bc32513d850b1816b
LD <LDinho@users.noreply.github.com>
2019-06-29 09:34:30 -0400

    add insert new user helper function & passes adding new user test

--- users/usersModel.js
+++ users/usersModel.js
@@ -2,10 +2,17 @@ const db = require('./../data/dbConfig');
 
 module.exports = {
   getUsers,
-  // insert,
+  insert,
   // remove,
 }
 
 async function getUsers() {
   return db('users');
+}
+
+async function insert(user) {
+  const [id] = await db('users').insert(user);
+
+  return db('users').where({id}).first();
+
 }
