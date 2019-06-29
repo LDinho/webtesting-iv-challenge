635273ca890af843f3099ff1afefe30484c32b46
LD <LDinho@users.noreply.github.com>
2019-06-29 10:06:02 -0400

    add tests for getUsers & delete fn

--- users/usersModel.spec.js
+++ users/usersModel.spec.js
@@ -16,7 +16,50 @@ describe('User Model', () => {
       expect(users[0].name).toBe('sam')
     })
 
+    it('should add a user and return an id', async () =>  {
+      const user = await Users.insert({name: 'sam'});
+      expect(user.id).toBe(1)
+      expect(user.name).toBe('sam')
+    });
+
   })
-})
+
+
+  describe('getUsers fn', () => {
+
+    beforeEach(() => {
+      return db('users').truncate()
+    })
+
+    it('should retrieve all users', async () => {
+      await db('users').insert([
+        {name: 'jim'},
+        {name: 'mary'},
+      ])
+
+      const users = await Users.getUsers();
+
+      expect(users.length).toBe(2)
+      expect(users[0].name).toBe('jim')
+
+    });
+  });
+
+  describe('delete user fn', () => {
+    it('should delete user by id', async () => {
+      const [id] = await db('users').insert([
+        {name: 'joe'}
+      ])
+
+      await Users.remove(id);
+
+      const user = await Users.getUserById(id);
+
+      expect(user).toBeUndefined();
+
+    });
+  });
+
+});
 
 // remember to add ( --env=testing ) when running migrations/seeding to be in the testing environment
